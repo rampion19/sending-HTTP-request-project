73381c6e0e82beb195de03f1a8518c09572b0be9
rampion19 <princegupta.pg19@gmail.com>
2023-02-26 21:00:09 +0530

    used useEffect and callback for optimization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null)
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async function () {
     setIsLoading(true);
     setError(null);
 
@@ -34,8 +34,11 @@ function App() {
     }
 
     setIsLoading(false);
-  }
+  },[]);
 
+  useEffect(() => {
+    fetchMoviesHandler();
+  },[fetchMoviesHandler])
   let content = <p>No movies found</p>;
 
   if (isLoading) {
